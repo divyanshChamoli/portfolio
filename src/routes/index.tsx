ecb1721475999c6b7c542604dafd010c2f0266a4
Divyansh <divyanshd.chamoli@gmail.com>
2025-07-05 21:19:47 +0000

    feat: add fonts

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,5 +5,11 @@ export const Route = createFileRoute('/')({
 })
 
 function App() {
-  return <div className=" bg-red-500 ">Hello</div>
+  return <div className=' pl-20 pt-20 '>
+    <p className=' text-[60px] font-press-start '> Divyansh Chamoli </p>
+    <p className=' text-[60px] font-pixel-grid-semibold '> Divyansh Chamoli </p>
+    <p className=' text-[60px] font-pixel-grid-medium '> Divyansh Chamoli </p>
+    <p className=' text-[60px] font-pixel-grid-bold '> Divyansh Chamoli </p>
+    <p className=' text-[60px] font-pixel-grid-bold '> Journey through the </p>
+  </div>
 }
